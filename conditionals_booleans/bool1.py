b75d1524077722f48ae9b1c51b20b1b2e0f4c27a
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-05 10:25:59 -0400

    Add function to check if a number is even or odd
    First usage of boolean statements

---
+++ conditionals_booleans/bool1.py
@@ -0,0 +1,16 @@
+def main():
+    x = int(input("What is X? "))
+    if is_even(x):
+        print("X is even")
+    else:
+        print("X is odd")     
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
+
+
