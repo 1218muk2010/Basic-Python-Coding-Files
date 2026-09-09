04dec71c6d5816f27cd924d8030d5987bf213d43
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-09 19:17:10 -0400

    Add while loop to print 'Hey Ya!'
    This code will print 'Hey Ya!' once. The while loop runs while i is less than 6, starting from 3 and increments i by 1 each iteration.

---
+++ loops/while_loop2.py
@@ -0,0 +1,15 @@
+i = 3
+while i < 6:
+    print("Hey Ya!")
+    i += 1
+'''
+This code will print "Hey Ya!" three times. 
+The while loop will continue to run as long as i is less than 6.q
+'''
+
+'''
++= basically means "add the value on the right to the variable on the left and then assign the result back to the variable on the left".
+In this case, i starts at 3.
+In the first iteration of the loop, it prints "Hey Ya!" and then adds 3 to i, making i equal to 6.
+Since 6 is not less than 6, the loop will stop running after the first iteration, and "Hey Ya!" will only be printed once   
+'''
