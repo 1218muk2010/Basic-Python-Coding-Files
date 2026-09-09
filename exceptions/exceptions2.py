8c881f0ada1d780a84c40c2fc269a6f195fad82f
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-09 19:20:44 -0400

    Create exceptions2.py

---
+++ exceptions/exceptions2.py
@@ -0,0 +1,24 @@
+while True:
+    try:
+        x = int(input("Enter a number: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+    else:
+        break
+
+print(f"Thank you for entering a valid number. X = {x}")
+
+
+
+'''
+You could also do this
+
+while True:
+    try:
+        x = int(input("Enter a number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+print(f"Thank you for entering a valid number. X = {x}")
+
+'''
