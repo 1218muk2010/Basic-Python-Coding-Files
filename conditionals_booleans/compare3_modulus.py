5f87047f5eb25cced4807004f7ccdf1567ebd937
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-09 19:09:11 -0400

    Add even/odd checker using modulus operator
    Implement a program to check if a number is even or odd using modulus - similar ideas to compare1 and compare2

---
+++ conditionals_booleans/compare3_modulus.py
@@ -0,0 +1,10 @@
+x = int(input("What is X? "))
+'''
+% is the modulus operator, it gives the remainder of the division of x by 2. If the remainder is 0, then x is even, otherwise it is odd.
+
+'''
+if x % 2 == 0:
+
+    print("X is even")
+else:
+    print("X is odd")
