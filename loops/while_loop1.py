08981ae70bc27695b5ebcfcbb8d38870a3af05d0
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-09 19:16:47 -0400

    Add while loop to print 'Hi' three times
    This code will print 'Hi' three times. The while loop will continue to run as long as 1 is not equal to 0, which is always true.

---
+++ loops/while_loop1.py
@@ -0,0 +1,13 @@
+i = 3
+while 1 != 0:
+    print("Hi")
+    i = i - 1
+    if i == 0:
+        break
+
+#This code will print "Hi" three times
+#The while loop will continue to run as long as 1 is not equal to 0, which is always true.
+'''
+Inside the loop, we print "Hi" and then decrease the value of i by 1.
+When i reaches 0, we break out of the loop, which stops the program from running indefinitely.
+'''
