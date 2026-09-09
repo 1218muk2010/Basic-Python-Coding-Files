b98160cd6df12881a1c6f53f3abd887b7420fcb6
Mukundh Sivaramakrishnan <mukbusiness112@gmail.com>
2026-09-09 19:13:08 -0400

    Implement loop to print 'Hello World!'
    Add a for loop to print 'Hello World!' three times.

---
+++ loops/a_list1.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    print("Hello World!")
+
+'''
+Range basically generates a sequence of numbers. In this case, range(3) generates the numbers 0, 1, and 2. 
+The for loop iterates over this sequence, and for each number (0, 1, and 2), it executes the block of code indented under it, which prints "Hello World!" three times.
+ The variable i takes on the value of each number in the sequence during each iteration of the loop, but in this case, it is not used inside the loop body.
+'''
